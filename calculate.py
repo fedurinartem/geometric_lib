70e01106b4c796f57857708a6db6240d55e978c0
Артем Федурин <fedurinartem.6@gmail.com>
2024-11-20 19:14:59 +0300

    edited

--- calculate.py
+++ calculate.py
@@ -1,33 +1,51 @@
 import circle
 import square
-
-
+	
 figs = ['circle', 'square']
 funcs = ['perimeter', 'area']
 sizes = {}
 
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
+	if fig not in figs:
+		raise ValueError(
+			f"Invalid figure: {fig}. Avaliable figures are: {figs}"
+		)
+	if func not in funcs:
+		raise ValueError(
+			f"Invalid function: {func}. Avaliable figures are: {funcs}"
+		)
+	if any(s<0 for s in size):
+		raise ValueError("Can't use negative values for figure dimensions.")
+	if fig =='circle':
+		if func=='perimeter':
+			return circle.perimeter(size[0])
+		elif func=='area':
+			return circle.area(size[0])
+	elif fig=='square':
+		if func=='perimeter':
+			return square.perimeter(size[0])
+		elif func=='area':
+			return square.area(size[0])
+try:
+	figure='circle'
+	function='area'
+	dimensions=[5]
+	result=calc(figure,function,dimensions)
+	print(
+		f"The {function} of the {figure} with dimension {dimensions} "
+		f"is: {result}"
+	)
+	figure='square'
+	function='perimeter'
+	dimensions=[4]
+	result=calc(figure,function,dimensions)
+	print(
+		f"The {function} of the {figure} with dimension {dimensions} "
+		f"is: {result}"
+	)
+except ValueError as e:
+	print(e)
 
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
-
-if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
-    
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	calc(fig, func, size)
 
 
 
