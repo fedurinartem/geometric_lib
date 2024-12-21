d893acad981398ab64f1865625196f276e870360
Артем Федурин <artemfedurin.6@gmail.com>
2024-12-21 09:55:21 +0300

    format

--- calculate.py
+++ calculate.py
@@ -1,29 +1,27 @@
 import circle
 import square
 
-figs = ['circle', 'square']
-funcs = ['perimeter', 'area']
+figs = ["circle", "square"]
+funcs = ["perimeter", "area"]
 sizes = {}
 
 
 def calc(fig, func, size):
     if fig not in figs:
-        raise ValueError(
-            f"Invalid figure: {fig}. Available figures are: {figs}"
-        )
+        raise ValueError(f"Invalid figure: {fig}."
+                         "Available figures are: {figs}")
     if func not in funcs:
-        raise ValueError(
-            f"Invalid function: {func}. Available functions are: {funcs}"
-        )
+        raise ValueError(f"Invalid function: {func}."
+                         "Available functions are: {funcs}")
     if any(s < 0 for s in size):
         raise ValueError("Can't use negative values for figure dimensions.")
-    if fig == 'circle':
-        if func == 'perimeter':
+    if fig == "circle":
+        if func == "perimeter":
             return circle.perimeter(size[0])
-        elif func == 'area':
+        elif func == "area":
             return circle.area(size[0])
-    elif fig == 'square':
-        if func == 'perimeter':
+    elif fig == "square":
+        if func == "perimeter":
             return square.perimeter(size[0])
-        elif func == 'area':
+        elif func == "area":
             return square.area(size[0])
