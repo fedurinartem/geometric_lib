cb3383f60ed074ba1ec76b3ba50167ed2b074309
Артем Федурин <artemfedurin.6@gmail.com>
2024-12-21 10:02:18 +0300

    format

--- calculate.py
+++ calculate.py
@@ -8,11 +8,14 @@ sizes = {}
 
 def calc(fig, func, size):
     if fig not in figs:
-        raise ValueError(f"Invalid figure: {fig}."
-                         "Available figures are: ['circle', 'square'")
+        raise ValueError(
+            f"Invalid figure: {fig}." "Available figures are: ['circle', 'square'"
+        )
     if func not in funcs:
-        raise ValueError(f"Invalid function: {func}."
-                         "Available functions are: ['perimeter', 'area']")
+        raise ValueError(
+            f"Invalid function: {func}."
+            "Available functions are: ['perimeter', 'area']"
+        )
     if any(s < 0 for s in size):
         raise ValueError("Can't use negative values for figure dimensions.")
     if fig == "circle":
